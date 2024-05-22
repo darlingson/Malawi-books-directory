773490e2f2fb4439765aadd293f491aa81ad2793
darlingson <dmakuwila@gmail.com>
2024-05-22 21:00:42 +0200

    configured project to use Pomelo

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Malawi_books_directory.Data;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 23))));
+
 
 var app = builder.Build();
 
