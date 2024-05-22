a7b96b256855982adef6f48f650bb0ce66ac1299
darlingson <dmakuwila@gmail.com>
2024-05-22 09:48:54 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Malawi_books_directory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
